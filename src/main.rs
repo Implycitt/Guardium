630375cc46008e58de1b4ae19b05c23cab1627df
Implycitt <qgbordelon@gmail.com>
2024-03-17 22:35:27 -0500

    Some more broken code to add to the pile

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,33 @@ use macroquad::prelude::*;
 #[macroquad::main("CSPFinal")]
 
 async fn main() {
-    loop {
-        let mut speed = 10.0; // probably should have a speed variable to move
 
+    let mut player = draw_rectangle(screen_width() / 2.0, screen_height() / 2.0, 10.0, 10.0, GREEN);
+    let mut speed = 10.0; // probably should have a speed variable to move
+
+    loop {
         clear_background(BLACK);
-        let player = draw_rectangle(xscreen_width() / 2.0, screen_height() / 2.0, 10.0, 10.0, GREEN);
+
+        let mut player_pos = vec2(screen_width() / 2.0, screen_height() / 2.0);
 
         if is_key_down(KeyCode::W) {
             // move up 
-            player.y += speed;
+            player_pos.y += speed;
         }
         if is_key_down(KeyCode::S) {
             // move down 
-            player.y -= speed;
+            player_pos.y -= speed;
         }
         if is_key_down(KeyCode::A) {
             // move left 
-            player.x -= speed;
+            player_pos.x -= speed;
         }
         if is_key_down(KeyCode::D) {
             // move right
-            player.x += speed;
+            player_pos.x += speed;
         }
+
+
 
         next_frame().await; // I think this draws
     }
