86e0a74d429a3867eee450a9c17de6398c5c2c2b
Implycitt <qgbordelon@gmail.com>
2024-03-19 22:22:05 -0500

    Migrated to using Bevy engine

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
-use macroquad::prelude::*;
+use bevy::{
+    core_pipeline::{
+        bloom::{BloomCompositeMode, BloomSettings},
+        tonemapping::Tonemapping,
+    },
+    prelude::*,
+    sprite::MaterialMesh2dBundle,
+};
 
-// creates the actual window and and gives it a name
-#[macroquad::main("CSPFinal")]
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
 
-async fn main() {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn((
+        Camera2dBundle {
+            camera: Camera {
+                hdr: true, // 1. HDR is required for bloom
+                ..default()
+            },
+            tonemapping: Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
+            ..default()
+        },
+        BloomSettings::default(), // 3. Enable bloom for the camera
+    ));
 
-    let mut player = draw_rectangle(screen_width() / 2.0, screen_height() / 2.0, 10.0, 10.0, GREEN);
-    let mut speed = 10.0; // probably should have a speed variable to move
+    // Circle mesh
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Circle::new(10.)).into(),
+        // 4. Put something bright in a dark environment to see the effect
+        material: materials.add(Color::rgb(7.5, 0.0, 7.5)),
+        // transform: Transform::from_translation(Vec3::new(-200., 0., 0.)),
+        // the idea of the line above is just to move or transform the Circle
+        // to the left 200 pixels
+        ..default()
+    });
+}
 
-    loop {
-        clear_background(BLACK);
-
-        let mut player_pos = vec2(screen_width() / 2.0, screen_height() / 2.0);
-
-        if is_key_down(KeyCode::W) {
-            // move up 
-            player_pos.y += speed;
-        }
-        if is_key_down(KeyCode::S) {
-            // move down 
-            player_pos.y -= speed;
-        }
-        if is_key_down(KeyCode::A) {
-            // move left 
-            player_pos.x -= speed;
-        }
-        if is_key_down(KeyCode::D) {
-            // move right
-            player_pos.x += speed;
-        }
-
-
-
-        next_frame().await; // I think this draws
+fn move_player(keys: Res<Input<KeyCode>>) {
+    if keys.just_pressed(KeyCode::W) {
+        // move player, todo tomorrow.
     }
 }
