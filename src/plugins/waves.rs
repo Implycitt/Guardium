6b1642dd074d9ae599fffd981a9ae9333657ff42
Implycitt <qgbordelon@gmail.com>
2024-04-18 11:45:26 -0500

    Some waves code gtg

--- src/plugins/waves.rs
+++ src/plugins/waves.rs
@@ -1,4 +1,12 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    utils::HashMap,
+};
+
+use crate::plugins::{
+    enemies::EnemyBundle,
+    state::GameState,
+};
 
 pub struct WavePlugin;
 
@@ -7,10 +15,32 @@ impl Plugin for WavePlugin {
     }
 }
 
-fn get_enemy_number() -> i32 {
-    todo!();
+#[derive(Resource, Default)]
+pub struct Waves {
+    pub waves: Vec<Wave>,
+    pub current: usize,
 }
 
-fn get_wave_time() -> i32 {
-    todo!();
+impl Waves {
+    pub fn current(&self) -> Option<&Wave> {
+        self.waves.get(self.current)
+    }
+    pub fn advance(&mut self) -> Option<&Wave> {
+        self.current += 1;
+        self.current()
+    }
+}
+
+#[derive(Clone)]
+pub struct Wave {
+    pub num: usize,
+    pub interval: f32,
+    pub delay: f32,
+}
+
+#[derive(Component)]
+pub struct WaveState {
+    pub delay_timer: Timer,
+    pub spawn_timer: Timer,
+    pub remaining: usize,
 }
